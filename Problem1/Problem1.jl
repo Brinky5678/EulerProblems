e0c0c956b7f00b56b110defe6140f3a76ea9aec1
Dennis Brinkman <dennisbrinkman93@gmail.com>
2017-10-07 00:21:28 +0200

    Updated Problem1

--- Problem1/Problem1.jl
+++ Problem1/Problem1.jl
@@ -1,17 +1,38 @@
+
+#General solution
 N = 1000
 divisors = [3, 5]
 SumElements = Vector{Int64}()
 
-for div in divisors
-    #Initial divisor is always included
-    val = div
-    #Loop untill number is reached
-    mult = 2
-    while val < N
-        push!(SumElements, val)
-        val = mult*div
-        mult += 1
+function SumDivisors(N::Int64, Divisors::Vector{Int64})
+    sumval = 0
+    for i=1:N-1
+        if any(rem.(i,Divisors) .== 0)
+            sumval += i
+        end
     end
+    return sumval
 end
 
-ANS = sum(SumElements)
+FirstSolutionTime = Vector{Float64}()
+for i=1:1000
+    tic()
+    Problem1Answer = SumDivisors(N,divisors)
+    push!(FirstSolutionTime,toq())
+end
+AvgTimeTakenFirstSolution = mean(FirstSolutionTime)
+
+#The more standard solution
+N = 1000
+SecondSolutionTime = Vector{Float64}()
+for i=1:1000
+    tic()
+sumval = 0
+for x=1:N-1
+    if (x%3 == 0 || x%5 == 0)
+        sumval += x
+    end
+end
+push!(SecondSolutionTime,toq())
+end
+AvgTimeTakenSecondSolution = mean(SecondSolutionTime)
